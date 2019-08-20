d0315ff1f76ce6115809ef9a3b1d202804552d41
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-19 21:23:03 -0400

    successfully scrape news outlet

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var mongojs = require('mongojs');
 var logger = require('morgan');
-var path = require('path');
+var axios = require('axios');
+var cheerio = require('cheerio');
+
 var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
+app.get('/scrape', function(req, res) {
+  axios.get('https://www.oceannews.com/').then(function(response) {
+    var $ = cheerio.load(response.data)
+
+    $('.title').each(function(i, element) {
+      var result = {};
+      result.title = $(this).children('a').text();
+      result.link = $(this).children('a').attr('href');
+
+      console.log(result);
+    })
+  });
+});
 
 require("./routes/htmlRoutes.js")(app)
 
