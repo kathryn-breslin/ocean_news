e88cd36cd298a282481e309b3e3211824e606da7
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-20 21:23:14 -0400

    data-id for comment button

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -3,65 +3,55 @@ function showArticles() {
 
   $.getJSON("/articles", function(data) {
     for (var i = 0; i < data.length; i++) {
-        var cardDiv = $('<div>');
-            cardDiv.addClass('card');
-            cardDiv.addClass('new-article-div');
+      var cardDiv = $("<div>");
+      cardDiv.addClass("card");
+      cardDiv.addClass("new-article-div");
 
-        var cardHeader = $('<h5>');
-            cardHeader.addClass('card-header');
-        
-        var cardBody = $('<div>');
-            cardBody.addClass('card-body');
+      var cardHeader = $("<h5>");
+      cardHeader.addClass("card-header");
 
-        var cardTitle = $('<h5>');
-            cardTitle.addClass('card-title');
-            cardTitle.attr('data-id=', data[i._id]);
-            cardTitle.append('<h5>' + data[i].title + '</h5>');
-        
-        var cardImage = $('<img src="' + data[i].image + '">');
-            cardImage.addClass('linkImage', 'card-img-top');
+      var cardBody = $("<div>");
+      cardBody.addClass("card-body");
 
-        var link = $('<a href="https://www.oceannews.com' + data[i].link + '">Go to Article</a>');
-            link.addClass('btn btn-primary');
-            link.addClass('articleButton');
-            link.addClass('float-right')
-            link.attr('target', '_blank');
-        
-        var button = $('<input type="button" value="Add Comment"/>');
-            button.addClass('btn btn-primary');
-            button.addClass('commentButton');
-            button.addClass('float-right')
+      var cardTitle = $("<h5>");
+      cardTitle.addClass("card-title");
+      cardTitle.attr("data-id=", data[i._id]);
+      cardTitle.append("<h5>" + data[i].title + "</h5>");
 
+      var cardImage = $('<img src="' + data[i].image + '">');
+      cardImage.addClass("linkImage", "card-img-top");
 
-        
-        cardBody.append(cardImage);
-        cardBody.append(cardTitle);
-        cardBody.append(link);
-        cardBody.append(button);
-        cardDiv.append(cardHeader);
-        cardDiv.append(cardBody);
+      var link = $(
+        '<a href="https://www.oceannews.com' +
+          data[i].link +
+          '">Go to Article</a>'
+      );
+      link.addClass("btn btn-primary");
+      link.addClass("articleButton");
+      link.addClass("float-right");
+      link.attr("target", "_blank");
 
-        $('#articles').append(cardDiv);
-    //   $("#articles").append(
-    //     "<p class='data-entry' data-id=" +
-    //       data[i]._id +
-    //       "><span class='dataTitle' data-id=" +
-    //       data[i]._id +
-    //       ">" +
-    //       data[i].title +
-    //       "</span><span class=delete>X</span></p>"
-    //   );
+      var button = $('<input data-id="' + data[i]._id + '" type="button" value="Add Comment"/>');
+      button.addClass("btn btn-primary");
+      button.addClass("commentButton");
+      button.addClass("float-right");
+
+      cardBody.append(cardImage);
+      cardBody.append(cardTitle);
+      cardBody.append(link);
+      cardBody.append(button);
+      cardDiv.append(cardHeader);
+      cardDiv.append(cardBody);
+
+      $("#articles").append(cardDiv);
     }
   });
 }
-
 showArticles();
 
-{/* <div class="card">
-  <h5 class="card-header">Featured</h5>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
+$(document).on('click', ".commentButton", function(){
+    var commentId = $(this).attr('data-id');
+
+    console.log("A comment has been made with ID: " + commentId);
+
+});
