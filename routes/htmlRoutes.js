b89dc50b762a50e47d5bd3b261d00b164b89a195
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-20 20:09:35 -0400

    add saved html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/index.html"));
     });
+
+    app.get('/saved', function(res, res) {
+        res.sendFile(path.join(__dirname, "../public/views/saved.html"));
+    });
 }
